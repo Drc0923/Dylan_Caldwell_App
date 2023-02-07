f90941c77400fe172e4f316f81119963b3ac6a3c
Drc0923 <drc0010@mix.wvu.edu>
2023-02-06 20:40:53 -0500

    Added cards and albums for listings on airbnb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './Navigation/top-nav-bar/top-nav-bar.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
