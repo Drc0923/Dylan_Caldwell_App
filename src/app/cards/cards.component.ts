94c9437e0637d15704dcf1f3656a1f6dc2205c8b
Drc0923 <drc0010@mix.wvu.edu>
2023-02-20 22:07:39 -0500

    Added top bar, as well as fiddled with current code. Tried to add data binding but am experiencing issues with using the tags from ts file

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+ @Input() img: string;
+ @Input() rating: number;
+ @Input() views: number;
+ @Input() dates: string;
+ @Input() price: number;
 
+  constructor() {
+    this.img = "";
+    this.rating = 0;
+    this.views = 0;
+    this.dates = "Missing Dates";
+    this.price = 0;
+
+  }
 }
