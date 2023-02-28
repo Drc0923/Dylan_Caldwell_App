72029e6787085d3aa069f16fb121402ac7ba09aa
Drc0923 <drc0010@mix.wvu.edu>
2023-02-27 19:55:29 -0500

    power might go out, adding components and inplemented data binding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './Navigation/top-nav-bar/top-nav-bar.component';
 import { CardsComponent } from './cards/cards.component';
+import { BottomNavbarComponent } from './bottom-navbar/bottom-navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    CardsComponent
+    CardsComponent,
+    BottomNavbarComponent,
+
   ],
   imports: [
     BrowserModule
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -7,12 +7,39 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
-  img: string;
+
+  slides: any[] = new Array(3).fill({id: -1, src:'', title: '', subtitle: ''})
+
+  @Input() img: string;
   @Input() price: number;
+  @Input() rating: number;
+  @Input() views: string;
+  @Input() dates: string;
+  @Input() location: string;
+  @Input() locationalt: string;
+  @Input() link: string;
 
 
     constructor() {
+      this.location = "";
       this.img = "";
       this.price= 0;
+      this.rating = 0;
+      this.views = "";
+      this.dates = "";
+      this.locationalt = "";
+      this.link = "";
+    }
+
+    ngOnInit(): void {
+      this.slides[0] = {
+        src: './assets/img/angular.jpg',
+      };
+      this.slides[1] = {
+        src: './assets/img/react.jpg',
+      }
+      this.slides[2] = {
+        src: './assets/img/vue.jpg',
+      }
     }
 }
